cab42854e28d2eb516f8edaec95d956e6dff6941
akash chaudhary <ak9838182955@gmail.com>
2024-04-18 15:52:19 +0530

    add multiple images

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,67 @@
 import React, { useState } from 'react';
 import firstpage from './1stpage.jpeg';
+import secondpage from './second.jpeg';
+import thirdpage from './third.jpeg';
+import fourth from './fourth.jpeg';
+import fifth from './fifth.jpeg';
+import six from './six.jpeg';
+import seven from './seven.jpeg';
+
 
 const BirthdayCard = () => {
     const pages = [
         {
-          title: "Happy Birthday Bhanji",
-          content: "To my sweet niece on her 1st birthday: You've brought so much happiness into our lives. Here's to many more years of love and laughter."
+          title: "Happy Birthday Cute Bhanji",
+          content: "To my sweet niece on her 1st birthday: You've brought so much happiness into our lives. Here's to many more years of love and laughter.",
+          foot: "Akash"
         },
         {
             title: "",
             content: "",
-            image: firstpage
+            image: firstpage,
+            foot: ""
+        },
+        {
+            title: "",
+            content: "",
+            image: secondpage,
+            foot: ""
+        },
+        {
+            title: "",
+            content: "",
+            image: thirdpage,
+            foot: ""
+        },
+        {
+            title: "",
+            content: "",
+            image: fourth,
+            foot: ""
+        },
+        {
+            title: "",
+            content: "",
+            image: fifth,
+            foot: ""
+        },
+        {
+            title: "",
+            content: "",
+            image: six,
+            foot: ""
+        },
+        {
+            title: "",
+            content: "",
+            image: six,
+            foot: ""
+        },
+        {
+            title: "",
+            content: "",
+            image: seven,
+            foot: ""
         },
         // Add more pages as needed
     ];
@@ -33,12 +84,17 @@ const BirthdayCard = () => {
                 </div>
             </div>
             <div className="cardInside">
-                <h3 className="back">{pages[currentPage].title}</h3>
+            {pages[currentPage].title && <h3 className="back">{pages[currentPage].title}</h3>}
+            
                 <p>{pages[currentPage].content}</p>
                 {currentPage === 1 && <img src={pages[currentPage].image} alt="Niece's 1st birthday" />}
                 {currentPage === 2 && <img src={pages[currentPage].image} alt="Niece's 1st birthday" />}
-                {currentPage === 2 && <img src={pages[currentPage].image} alt="Niece's 1st birthday" />}
-                <p className="name">Akash</p>
+                {currentPage === 3 && <img src={pages[currentPage].image} alt="Niece's 1st birthday" />}
+                {currentPage === 4 && <img src={pages[currentPage].image} alt="Niece's 1st birthday" />}
+                {currentPage === 5 && <img src={pages[currentPage].image} alt="Niece's 1st birthday" />}
+                {currentPage === 6 && <img src={pages[currentPage].image} alt="Niece's 1st birthday" />}
+                {currentPage === 7 && <img src={pages[currentPage].image} alt="Niece's 1st birthday" />}
+                {pages[currentPage].foot && <p className="name">{pages[currentPage].foot}</p>}
             </div>
         </div>
     );
